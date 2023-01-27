70c4bc1922cdc8f3c7521ea9ff56369f8d7d0b7c
Ebubekir <ebubekirn96@gmail.com>
2023-01-27 19:31:33 +0300

    Yeni projeye devam edildi.

---
+++ SignalRServerExample/Hubs/MyHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServerExample.Hubs
+{
+	public class MyHub : Hub
+	{
+		public async Task SendMessageAsync(string message)
+		{
+			await Clients.All.SendAsync("receiveMessage", message); // receiveMessage isimli bir fonksiyonu client ta tetikle ve client ın bana message olarak gönderdiği parametreyi diğer tüm client lara gönder.
+		}
+	}
+}
